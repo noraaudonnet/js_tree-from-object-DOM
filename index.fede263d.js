d4611a02913aa42df72fea42bbf9ddca68b306c5
Nora Audonnet <noraaudonnet@gmail.com>
2024-06-12 16:46:30 +0300

    make build

---
+++ index.fede263d.js
@@ -0,0 +1,2 @@
+!function e(n,t){if(0!==Object.keys(t).length){var r=document.createElement("ul");for(var a in t){var c=document.createElement("li");c.textContent="".concat(a),e(c,t[a]),r.append(c)}n.append(r)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.fede263d.js.map
